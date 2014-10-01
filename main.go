af10f8279a34ecef3ba897a9c69ac2ed70a24670
Aitor González Fernández <reimashi@gmail.com>
2014-10-01 19:32:17 +0200

    Creado el método para generar pares de claves Rsa

--- main.go
+++ main.go
@@ -9,21 +9,42 @@ func main() {
 	if len(os.Args) >= 2 {
 		switch os.Args[1] {
 		case "genkeys":
-			calcularClaves()
+			cmdGenkeys()
 		case "help":
 			cmdHelp()
 		default:
-			fmt.Println("Command " + os.Args[1] + "incorrect. Check help with \"help\" command.")
+			fmt.Println("Comando <" + os.Args[1] + "> incorrecto. Comprueba la ayuda con el comando \"help\".")
 		}
 	} else {
 		cmdHelp()
 	}
 }
 
+func cmdGenkeys() {
+	if len(os.Args) >= 3 {
+		path, err := os.Getwd()
+		if err != nil {
+			fmt.Println("<Error interno> No se ha podido determinar el directorio actual.")
+			return
+		}
+
+		err = GenerateRsaKeys(path, os.Args[2])
+		if err != nil {
+			fmt.Println("<Error interno> Las llaves no han podido ser generadas.")
+			return
+		}
+
+		fmt.Println("Las llaves han sido creadas con éxito.")
+	} else {
+		fmt.Println("Los argumentos no son suficientes:")
+		fmt.Println("compostelas genkeys <usuario>")
+	}
+}
+
 func cmdHelp() {
-	fmt.Println("compostelas <command> [Options]")
+	fmt.Println("compostelas <comando> [Opciones]")
 	fmt.Println()
-	fmt.Println("Commands:")
-	fmt.Println("\tgenkeys <user>\tGenerate public/private keys for <user>.")
-	fmt.Println("\thelp\t\tShow this help.")
+	fmt.Println("Comandos:")
+	fmt.Println("\tgenkeys <usuario>\tGenera un par de llaves publicas/privadas para el <usuario>.")
+	fmt.Println("\thelp\t\tMuestra este dialogo de ayuda.")
 }
---
+++ rsa_manager.go
@@ -0,0 +1,72 @@
+package compostelas
+
+import (
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/x509"
+	"encoding/pem"
+	"errors"
+	"os"
+)
+
+func GenerateRsaKeys(dir string, name string) error {
+	directory, err := os.Stat(dir)
+	if err != nil {
+		return err
+	} else if !directory.IsDir() {
+		return errors.New("Directory path isn't correct.")
+	}
+
+	privateKey, err := rsa.GenerateKey(rand.Reader, 2048)
+	if err != nil {
+		return err
+	}
+
+	privateKeyByteCode := x509.MarshalPKCS1PrivateKey(privateKey)
+	privateKeyBlock := pem.Block{
+		Type:    "RSA PRIVATE KEY",
+		Headers: nil,
+		Bytes:   privateKeyByteCode,
+	}
+
+	publicKeyByteCode, err := x509.MarshalPKIXPublicKey(&privateKey.PublicKey)
+	if err != nil {
+		return err
+	}
+
+	publicKeyBlock := pem.Block{
+		Type:    "RSA PUBLIC KEY",
+		Headers: nil,
+		Bytes:   publicKeyByteCode,
+	}
+
+	filepublic, err := os.Create(directory.Name() + "/" + name + ".public")
+	if err != nil {
+		return err
+	}
+
+	defer filepublic.Close()
+
+	_, err = filepublic.Write([]byte(pem.EncodeToMemory(&publicKeyBlock)))
+	if err != nil {
+		return err
+	}
+
+	filepublic.Sync()
+
+	fileprivate, err := os.Create(directory.Name() + "/" + name + ".private")
+	if err != nil {
+		return err
+	}
+
+	defer fileprivate.Close()
+
+	_, err = fileprivate.Write([]byte(pem.EncodeToMemory(&privateKeyBlock)))
+	if err != nil {
+		return err
+	}
+
+	fileprivate.Sync()
+
+	return nil
+}
