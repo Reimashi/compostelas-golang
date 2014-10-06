1496d803d23973a140617c98414be95d54a0ce52
Aitor González Fernández <reimashi@gmail.com>
2014-10-06 22:16:22 +0200

    Creado el objeto Compostela, para manipular el archivo con la compostela.

---
+++ compostela.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"container/list"
+	"errors"
+)
+
+type compostelaState struct (
+	pilgrimEncrypted bool
+)
+
+type compostelaData struct (
+	Pilgrim []byte
+	Stamps list.List
+)
+
+type Compostela struct (
+	Data compostelaData
+	State compostelaState
+)
+
+/*
+Obtiene un objeto Compostela desde un archivo.
+*/
+func GetCompostelaFromFile (path string) Compostela, error {
+	comp := Compostela{}
+	
+	file, err := ioutil.ReadFile(string)
+	if err != nil {
+		return comp, err
+	}
+	
+	comp.State.pilgrimEncrypted = true
+	
+    err = json.Unmarshal(file, &comp.Data)
+	if err != nil {
+		return comp, err
+	}
+	
+	return comp, nil
+}
