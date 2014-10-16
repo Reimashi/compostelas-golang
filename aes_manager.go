d2a1769c283dfaa696c3cbff685ba84331dccaa0
Aitor González Fernández <reimashi@gmail.com>
2014-10-16 13:07:58 +0200

    Modificada la encriptación AES

--- aes_manager.go
+++ aes_manager.go
@@ -3,26 +3,45 @@ package main
 import (
 	"crypto/aes"
 	"crypto/cipher"
+	"encoding/hex"
 )
 
-func encriptar_aes(data []byte, key []byte, iv []byte) []byte {
+var default_iv string = "22277966616d9bc47177bd02603d08c9a67d5380d0fe8cf3b44438dff7b9"
+
+func EncryptAes(data []byte, key string) ([]byte, error) {
 	encrypted := make([]byte, len(data))
+
+	iv, err := hex.DecodeString(default_iv)
+	if err != nil {
+		return encrypted, err
+	}
+
 	aesBlockEncrypter, err := aes.NewCipher([]byte(key))
 	if err != nil {
-		panic(err)
+		return encrypted, err
 	}
+
 	aesEncrypter := cipher.NewCFBEncrypter(aesBlockEncrypter, iv)
 	aesEncrypter.XORKeyStream(encrypted, data)
-	return encrypted
+
+	return encrypted, nil
 }
 
-func desencriptar_aes(data []byte, key []byte, iv []byte) []byte {
+func DecryptAes(data []byte, key string) ([]byte, error) {
 	decrypted := make([]byte, len(data))
+
+	iv, err := hex.DecodeString(default_iv)
+	if err != nil {
+		return decrypted, err
+	}
+
 	aesBlockDecrypter, err := aes.NewCipher([]byte(key))
 	if err != nil {
-		panic(err)
+		return decrypted, err
 	}
+
 	aesDecrypter := cipher.NewCFBDecrypter(aesBlockDecrypter, iv)
 	aesDecrypter.XORKeyStream(decrypted, data)
-	return decrypted
+
+	return decrypted, nil
 }
